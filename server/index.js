9730c8a876b59e17bc0559ec546284961f8c26ac
ArthurWang010203 <53921710+ArthurWang010203@users.noreply.github.com>
2021-01-02 15:29:40 -0600

    Create index.js
    init commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+//Arthur Wang & Andrew Wang 1/1/2021
+var express = require('express');
+var app = express();
+const port = 3000
+const router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+// mainDiction is dictionary of printers
+var mainDiction = {};
+// var mainDiction = {'10.0.0.141:MK3 Prusa':{'name':'10.0.0.141:MK3 Prusa', 'status':'Connected','printJobStarted':'1/1/2021','estimatedPrintTime':'','job':''},'10.0.0.115:Creality 3D Ender 3':{'name':'10.0.0.115:Creality 3D
+ Ender 3','status':'Available','printJobStarted':'','estimatedPrintTime':'','job':''}}
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+        var arr = [];
+        for(var key in mainDiction){
+                arr.push(mainDiction[key])
+        }
+        res.render('/home/pi/statusPage/index.ejs', {
+                printerInfo: arr
+        });
+});
+
+app.post('/', (request, response) => {
+        console.log(request.body);
+
+        var jsonRequest = request.body;
+        var name = jsonRequest.name;
+        mainDiction[name] = jsonRequest;
+        var jsonResponse = jsonRequest.name;
+        response.send(jsonResponse);
+});
+
+app.use('/', router);
+console.log('Operating on Port: '+port);
+app.listen(process.env.port || port);
