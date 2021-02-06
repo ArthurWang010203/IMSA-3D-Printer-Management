a40aeb756bf0da90a36f6987a3c022af66d1bf1b
ArthurWang010203 <53921710+ArthurWang010203@users.noreply.github.com>
2021-02-05 20:24:59 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 // mainDiction is dictionary of printers
 var mainDiction = {};
+const fs = require('fs')
 // var mainDiction = {'10.0.0.141:MK3 Prusa':{'name':'10.0.0.141:MK3 Prusa', 'status':'Connected','printJobStarted':'1/1/2021','estimatedPrintTime':'','job':''},'10.0.0.115:Creality 3D Ender 3':{'name':'10.0.0.115:Creality 3D Ender 3','status':'Available','printJobStarted':'','estimatedPrintTime':'','job':''}}
 
 app.use(bodyParser.json());
@@ -16,7 +17,13 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
         var arr = [];
         for(var key in mainDiction){
-                arr.push(mainDiction[key])
+		arr.push(mainDiction[key]);
+		if(mainDiction[key]['status']=='Available (Last Print Failed)'){
+			fs.appendFileSync('printLog.txt', (mainDiction[key]['name']+" "+mainDiction[key]['job']['user']+": "+mainDiction[key]['job']['file']['name']+" started at "+mainDiction[key]['printJobStarted']+" for "+mainDiction[key]['estimatedPrintTime']+": Failed\n\n"));
+		}
+		else if(mainDiction[key]['status'=='Available (Last Print Finished)']){
+			fs.appendFileSync('printLog.txt', (mainDiction[key]['name']+" "+mainDiction[key]['job']['user']+": "+mainDiction[key]['job']['file']['name']+" started at "+mainDiction[key]['printJobStarted']+" for "+mainDiction[key]['estimatedPrintTime']+": Failed\n\n"));
+		}
         }
         res.render('/home/pi/statusPage/index.ejs', {
                 printerInfo: arr
